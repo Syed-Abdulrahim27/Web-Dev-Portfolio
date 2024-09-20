b121fd3899f7331e6210d980fb82fe5e6fcb9e9c
Syed-Abdulrahim27 <syedabdulrahim27@gmail.com>
2024-09-20 22:53:42 +0500

    Initial commit

---
+++ js.js
@@ -0,0 +1,52 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.querySelector(".nav-links");
+const menuBtnIcon = document.querySelector("#menu-btn i");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute(
+    "class",
+    isOpen ? "fa-solid fa-times" : "fa-solid fa-bars"
+  );
+});
+
+navLinks.addEventListener("click", () => {
+  // Close menu when a nav link is clicked
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "fa-solid fa-bars");
+});
+
+// ScrollReveal options
+const scrollRevealOption = {
+  origin: "bottom",
+  distance: "50px",
+  duration: 1000,
+};
+ScrollReveal().reveal(".edu", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 500,
+});
+ScrollReveal().reveal(".body-2", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 500,
+});
+ScrollReveal().reveal(".Courses", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".Projects", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".Awards", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+ScrollReveal().reveal(".certifs", {
+  ...scrollRevealOption,
+  delay: 2500,
+});
