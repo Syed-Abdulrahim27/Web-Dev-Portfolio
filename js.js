b3d3cf4c8bb29771c60753e1422110d6e472848f
Syed-Abdulrahim27 <syedabdulrahim27@gmail.com>
2024-09-20 23:43:49 +0500

    Making Javascript object consisting of Data used in html file

--- js.js
+++ js.js
@@ -49,3 +49,39 @@ ScrollReveal().reveal(".certifs", {
   ...scrollRevealOption,
   delay: 2500,
 });
+const profileData = {
+  name: "Syed Abdul Rahim",
+  job: "Web Developer in Pakistan",
+  profilePic:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT58-VVT8Wch6ligqL9BVGs4hHtZ2ChZeURvA&s",
+  socialLinks: {
+    instagram: "https://instagram.com",
+    linkedin: "https://linkedin.com",
+    github: "https://github.com",
+  },
+  education: [
+    "A-Levels from KIMS",
+    "BSCS in Computer Science",
+    "Masters in Computer Science",
+  ],
+  courses: ["HTML/CSS", "Javascript", "React", "Mobile App Development"],
+  projects: [
+    "Snake Game",
+    "Face Detection",
+    "Socket Programming",
+    "Flight Schedule Mobile App",
+  ],
+  awards: [
+    "Hackathon Prizes",
+    "Google Code Jam / Kick Start",
+    "Microsoft Imagine Cup",
+    "TopCoder Open",
+  ],
+  certifications: [
+    "Oracle Certified Professional: Java SE",
+    "Cloud Computing",
+    "Cybersecurity",
+    "Microsoft Certified: Azure AI Engineer Associate",
+  ],
+  portfolioVideo: "https://www.youtube.com/watch?v=Ez8F0nW6S-w",
+};
